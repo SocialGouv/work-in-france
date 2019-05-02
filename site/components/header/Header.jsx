570cdb721f4bfd8d5558bc16e48c016a2d92f9fc
kwiss <mr.dumont.christophe@gmail.com>
2019-05-02 10:17:39 +0200

    feat(site): next js work in france
    switch to next js
    
    BREAKING CHANGE: remove old site folder to new next js version

---
+++ site/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '/static/logi.png';
+
+const Header = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Header;
---
+++ site/pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="fr">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
