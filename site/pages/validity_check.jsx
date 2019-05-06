2cc93c98e22301049f09fbe6ea57d90a7b4d62e9
kwiss <mr.dumont.christophe@gmail.com>
2019-05-06 23:29:12 +0200

    refactor(pages): fix lint and extention

--- site/pages/validity_check.jsx
+++ site/pages/validity_check.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const validity_check = () => {
+const validityCheck = () => {
   return <div>validity_check</div>;
 };
 
-export default validity_check;
+export default validityCheck;
