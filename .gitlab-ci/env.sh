e14c0172b96b340d84fb438b2c2fb9076f4b6f61
Douglas Duteil <douglasduteil@gmail.com>
2019-06-25 19:34:29 +0200

    ci(gitlab): add basic gitlab groove (9)

--- .gitlab-ci/env.sh
+++ .gitlab-ci/env.sh
@@ -3,23 +3,33 @@
 #
 
 export BRANCH_NAME=${BRANCH_NAME:=$CI_COMMIT_REF_NAME}
+export COMMIT_TAG=${COMMIT_TAG:=$CI_COMMIT_TAG}
 export COMMIT=${COMMIT:=$CI_COMMIT_SHA}
+export ENVIRONMENT=${ENVIRONMENT:="dev.factory"};
+export HASH_SIZE=${HASH_SIZE:=7}
 export JOB_ID=${JOB_ID:=$CI_JOB_ID}
-export HASH_SIZE=${HASH_SIZE}
-BRANCH_NAME_HASHED=$(printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
+
+BRANCH_NAME_HASHED=$( printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
 export BRANCH_HASH=${BRANCH_HASH:=$BRANCH_NAME_HASHED}
 
 #
 
-case ${BRANCH_NAME} in
-  master)
-    export ENVIRONMENT=incubateur;
-    #
-    export FRONTEND_HOST=work-in-france.${ENVIRONMENT}.social.gouv.fr;
-    ;;
-  *)
-    export ENVIRONMENT=dev;
-    #
-    export FRONTEND_HOST=${BRANCH_HASH}.work-in-france.${ENVIRONMENT}.factory.social.gouv.fr;
-    ;;
-esac
+if [[ "${BRANCH_NAME}" = "master" ]]; then
+  export BRANCH_HASH=master;
+fi
+
+if [[ -n "${COMMIT_TAG}" ]]; then
+  export ENVIRONMENT=incubateur;
+  export BRANCH_HASH=$( printf "${COMMIT_TAG}" | sed "s/\./-/g" );
+fi
+
+export FRONTEND_HOST="${BRANCH_HASH}.work-in-france.${ENVIRONMENT}.factory.social.gouv.fr";
+
+#
+
+printenv | grep -E \
+  "BRANCH_HASH|BRANCH_NAME|COMMIT|COMMIT_TAG|ENVIRONMENT|HASH_SIZE|JOB_ID" \
+  | sort
+printenv | grep -E \
+  "FRONTEND_HOST" \
+  | sort
