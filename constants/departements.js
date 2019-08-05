e302f43dee289e0d9cd83231efbf36a58cced606
SiegridHenry <49791551+SiegridHenry@users.noreply.github.com>
2019-08-05 11:29:02 +0200

    Update departements.js

--- constants/departements.js
+++ constants/departements.js
@@ -6,7 +6,7 @@ const DEPARTEMENTS = [
     label: "02 - Aisne",
     company: "ud002-apt-etudiants-employeur",
     student: "ud002-apt-etudiants",
-  }, 
+  },
   { value: "03", label: "03 - Allier" },
   { value: "04", label: "04 - Alpes-de-Haute-Provence" },
   { value: "05", label: "05 - Hautes-alpes" },
