b11dd8b32af433365acdfc21f3ba6c86f51c4fe6
DanielBMY <35037792+DanielBMY@users.noreply.github.com>
2019-09-12 19:53:13 +0200

    Update faq.jsx

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -25,6 +25,11 @@ const Faq = () => {
                 </a>
               </li>
               <li>
+                 <a href="#quest-ce-quune-autorisation-provisoire-de-travail-pour-étudiant-étranger">
+                  Qu’est-ce qu’une autorisation provisoire de travail pour étudiant étranger ?
+                </a>
+              </li>
+              <li>
                 <a href="#en-combien-de-temps-puis-je-recevoir-mon-autorisation-provisoire-de-travail">
                   En combien de temps puis-je recevoir mon autorisation provisoire de travail ?
                 </a>
