fb0fffcee1c1011bca70eeb9e0821b3c88b71b5f
DanielBMY <35037792+DanielBMY@users.noreply.github.com>
2019-09-21 16:30:59 +0200

    Update ValidityCheck.jsx
    ajout de la mention qui évite que les usagers testent par ici leurs docs de séjour

--- components/validityCheck/ValidityCheck.jsx
+++ components/validityCheck/ValidityCheck.jsx
@@ -10,7 +10,7 @@ const ValidityCheck = () => {
         d'une autorisation provisoire de travail pour un étudiant étranger
       </Heading>
       <Text color="text" pb="4">
-        Disponible au plus tard 1 heure après délivrance de l'autorisation.
+        Disponible au plus tard 1 heure après délivrance de l'autorisation. Ne concerne que les autorisations délivrées via Work In France
       </Text>
       <ValidityCheckFormContainer />
     </Box>
