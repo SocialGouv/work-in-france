cec8bb83b5c7791eebe958f4b7cbbcfc44b32394
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-10-01 18:00:43 +0200

    chore: lintfix

--- components/validityCheck/ValidityCheck.jsx
+++ components/validityCheck/ValidityCheck.jsx
@@ -10,8 +10,8 @@ const ValidityCheck = () => {
         d'une autorisation provisoire de travail pour un étudiant étranger
       </Heading>
       <Text color="text" pb="4">
-        Disponible au plus tard 1 heure après délivrance de l'autorisation. Ne
-        concerne que les autorisations délivrées via Work In France
+        Disponible au plus tard 1 heure après délivrance de l'autorisation. Ne concerne que les
+        autorisations délivrées via Work In France
       </Text>
       <ValidityCheckFormContainer />
     </Box>
