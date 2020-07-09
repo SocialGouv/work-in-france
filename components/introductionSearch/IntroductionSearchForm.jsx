5684210b48589e8130356c39f37fc396b23e19ea
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-07-09 19:14:12 +0200

    feat(demarche-introduction): create page

--- components/introductionSearch/IntroductionSearchForm.jsx
+++ components/introductionSearch/IntroductionSearchForm.jsx
@@ -1,55 +1,47 @@
 // @flow
 import React, { useState } from "react";
 import Select from "react-select";
-import { Button, Flex, Box, Text, Link } from "rebass";
+import { Button, Flex, Box, Link } from "rebass";
 
 import { DEPARTEMENTS } from "../../constants/departements";
 
-const IntroductionSearchForm =  () => {
+const IntroductionSearchForm = () => {
   const activeDepartements = DEPARTEMENTS.filter(elm => elm.introduction);
-  const DEPARTEMENT_OPTIONS = activeDepartements.map(elm => ({label: elm.label, value: elm.value}));
+  const DEPARTEMENT_OPTIONS = activeDepartements.map(elm => ({
+    label: elm.label,
+    value: elm.value,
+  }));
 
   const [departement, setDepartement] = useState(false);
   const qualify = DEPARTEMENTS.find(dep => dep.value === departement.value);
   return (
-    <Flex
-              alignItems="center"
-              flexDirection="column"
-              flexWrap="wrap"
-              justifyContent="center"
-            >
-              <Box alignItems="center" pb={3} pt={1} width={[1, 1 / 2, 1 / 3]}>
-                <Select
-                  id="departement"
-                  isSearchable
-                  name="departement"
-                  onChange={option => setDepartement(option)}
-                  options={DEPARTEMENT_OPTIONS}
-                  placeholder="Département sur le titre de séjour"
-                  value={departement}
-                />
-              </Box>
-              {qualify &&
-              <Box alignItems="center" pb={0} pt={1} width={[1, 1 / 2, 1 / 3]}>
-              <Link
-                href={`https://www.demarches-simplifiees.fr/commencer/${qualify.introduction}`}
-                pt={2}
-                width={1}
-              >
-                <Button
-                  bg="blueBg"
-                  color="white"
-                  fontSize={2}
-                  py={2}
-                  width={[1]}
-                >
-                  Faire une introduction
-                </Button>
-                </Link>
-              </Box>
-              }
-            </Flex>
-  )
-}
+    <Flex alignItems="center" flexDirection="column" flexWrap="wrap" justifyContent="center">
+      <Box alignItems="center" pb={3} pt={1} width={[1, 1 / 2, 1 / 3]}>
+        <Select
+          id="departement"
+          isSearchable
+          name="departement"
+          onChange={option => setDepartement(option)}
+          options={DEPARTEMENT_OPTIONS}
+          placeholder="Département sur le titre de séjour"
+          value={departement}
+        />
+      </Box>
+      {qualify && (
+        <Box alignItems="center" pb={0} pt={1} width={[1, 1 / 2, 1 / 3]}>
+          <Link
+            href={`https://www.demarches-simplifiees.fr/commencer/${qualify.introduction}`}
+            pt={2}
+            width={1}
+          >
+            <Button bg="blueBg" color="white" fontSize={2} py={2} width={[1]}>
+              Faire une introduction
+            </Button>
+          </Link>
+        </Box>
+      )}
+    </Flex>
+  );
+};
 
 export default IntroductionSearchForm;
