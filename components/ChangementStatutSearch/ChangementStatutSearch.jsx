8995c1a790c40ee92d56fe8eade0b480dc18a411
Eric H <58169990+Eric-Hei@users.noreply.github.com>
2020-07-13 14:30:57 +0200

    Maj wording

--- components/ChangementStatutSearch/ChangementStatutSearch.jsx
+++ components/ChangementStatutSearch/ChangementStatutSearch.jsx
@@ -36,7 +36,7 @@ const IntroductionSearch = () => {
             textAlign="center"
             width={[1, 2 / 3]}
           >
-            {`Employeurs, Etudiants, demandez votre autorisation de travail pour recruter 
+            {`Etudiants étrangers, demandez votre autorisation de travail pour recruter 
             un salarié étranger résidant en France (procédure de changement de statut) en quelques clics`}
           </Text>
         </Flex>
