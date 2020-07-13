780897e897d5cad4076ca641843689972c167c8c
Eric H <58169990+Eric-Hei@users.noreply.github.com>
2020-07-13 15:09:58 +0200

    Maj wording

--- components/ChangementStatutSearch/ChangementStatutSearchForm.jsx
+++ components/ChangementStatutSearch/ChangementStatutSearchForm.jsx
@@ -35,7 +35,7 @@ const ChangementStatutSearchForm = () => {
             width={1}
           >
             <Button bg="blueBg" color="white" fontSize={2} py={2} width={[1]}>
-              Faire un changement de statut
+              Je fais un changement de statut
             </Button>
           </Link>
         </Box>
