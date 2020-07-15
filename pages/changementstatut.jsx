13ebd555940745642148d4971a511a9b0096124e
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-07-15 11:20:33 +0200

    feat(bandeau): add bandeau

--- pages/changementstatut.jsx
+++ pages/changementstatut.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "rebass";
 import Head from "next/head";
 import ChangementStatutSearch from "../components/ChangementStatutSearch";
 import ChangementStatutExplanation from "../components/ChangementStatutExplanations";
@@ -10,6 +11,9 @@ const ChangementStatutPage = () => {
         <title>Work in France - Votre demande de changement de statut</title>
       </Head>
       <ChangementStatutSearch />
+      <Text bg="blueBg" color="white" p="4" textAlign="center">
+        {`Le service est en test sur Paris.`}
+      </Text>
       <ChangementStatutExplanation />
     </>
   );
--- pages/introduction.jsx
+++ pages/introduction.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "rebass";
 import Head from "next/head";
 import IntroductionSearch from "../components/introductionSearch";
 import IntroductionExplanation from "../components/introductionExplanations";
@@ -10,6 +11,9 @@ const IntroductionPage = () => {
         <title>Work in France - Votre demande d'autorisation de travail</title>
       </Head>
       <IntroductionSearch />
+      <Text bg="blueBg" color="white" p="4" textAlign="center">
+        {`Le service est en test sur Paris.`}
+      </Text>
       <IntroductionExplanation />
     </>
   );
