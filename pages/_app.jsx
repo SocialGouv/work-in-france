6991e9a63df78419aa929bb5f21ce66c639524b4
Douglas Duteil <douglasduteil@gmail.com>
2020-10-08 14:15:12 +0200

    fix: migrate to next-redux-wrapper 6.x

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import App from "next/app";
 import { ThemeProvider } from "styled-components";
-import { Provider } from "react-redux";
-import withRedux from "next-redux-wrapper";
+import { createWrapper } from "next-redux-wrapper";
 import GlobalStyle from "../theme/globalStyle";
 import makeStore from "../lib/store";
 
@@ -11,34 +9,19 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import theme from "../theme/index";
 
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </>
+      </ThemeProvider>
+    </>
+  );
+};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps, store } = this.props;
-    return (
-      <>
-        <GlobalStyle />
-        <Provider store={store}>
-          <ThemeProvider theme={theme}>
-            <>
-              <Header />
-              <Component {...pageProps} />
-              <Footer />
-            </>
-          </ThemeProvider>
-        </Provider>
-      </>
-    );
-  }
-}
-
-export default withRedux(makeStore)(MyApp);
+export default createWrapper(makeStore).withRedux(MyApp);
