d8d4a14c2d7a0bf88fbeb02a15153ce669f5bd19
Douglas DUTEIL <douglasduteil@gmail.com>
2020-12-10 17:45:10 +0100

    build(k8s): update to @socialgouv/kosko-charts@^4.0.0-alpha.15

--- .k8s/__tests__/kosko generate --env preprod.ts
+++ .k8s/__tests__/kosko generate --env preprod.ts
@@ -1,8 +1,15 @@
 //
 
 import { getEnvManifests } from "@socialgouv/kosko-charts/testing";
+import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --preprod", async () => {
-  expect(await getEnvManifests("preprod")).toMatchSnapshot();
+  expect(
+    await getEnvManifests("preprod", "", {
+      ...project("serving-ml").preprod,
+      RANCHER_PROJECT_ID: "c-bd7z2:p-2qlgk",
+      KUBE_NAMESPACE: "work-in-france-50-preprod-dev2",
+    })
+  ).toMatchSnapshot();
 });
