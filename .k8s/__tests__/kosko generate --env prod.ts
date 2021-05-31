9301f8fa2057880256069f31983349bb3dbc84b0
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-31 13:46:43 +0200

    fix(ci): use kaniko+harbor

--- .k8s/__tests__/kosko generate --env prod.ts
+++ .k8s/__tests__/kosko generate --env prod.ts
@@ -5,6 +5,7 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --prod", async () => {
+  process.env.HARBOR_PROJECT = "wif";
   expect(
     await getEnvManifests("prod", "'!(_*)'", {
       ...project("work-in-france").prod,
--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,9 +1,13 @@
 import env from "@kosko/env";
 
 import { create } from "@socialgouv/kosko-charts/components/nginx";
+import { getHarborImagePath } from "@socialgouv/kosko-charts/utils"
 
 const manifests = create("app", {
-  env
+  env,
+  config:{
+    image: getHarborImagePath({ name:"wif-front" })
+  }
 });
 
 export default manifests;
